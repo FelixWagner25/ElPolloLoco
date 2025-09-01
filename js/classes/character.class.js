1f9c331ecac297d4a593fc6c15340f6d65243a3c
Felix Wagner <post@felixwagner.eu>
2025-09-01 15:10:46 +0200

    feat: load images to character and chicken

--- js/classes/character.class.js
+++ js/classes/character.class.js
@@ -1,3 +1,9 @@
 class Character extends MovableObject {
+  constructor() {
+    super().loadImage(
+      "../../img/img_pollo_locco/2_character_pepe/2_walk/W-21.png"
+    );
+  }
+
   jump() {}
 }
--- js/classes/chicken.class.js
+++ js/classes/chicken.class.js
@@ -1,1 +1,7 @@
-class Chicken extends MovableObject {}
+class Chicken extends MovableObject {
+  constructor() {
+    super().loadImage(
+      "../../img/img_pollo_locco/3_enemies_chicken/chicken_normal/1_walk/1_w.png"
+    );
+  }
+}
