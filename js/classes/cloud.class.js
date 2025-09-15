ac1752c289c50609fe7e2b626a9ab850d7db6aa5
Felix Wagner <post@felixwagner.eu>
2025-09-15 13:16:29 +0200

    feat: move clouds x-coordinate

--- js/classes/cloud.class.js
+++ js/classes/cloud.class.js
@@ -3,10 +3,15 @@ class Cloud extends MovableObject {
   y = 50;
   width = 360;
   height = 250;
+  v = 1;
+  dt = 60; // in MilliSeconds
 
   constructor() {
     super().loadImage(
       "../../img/img_pollo_locco/5_background/layers/4_clouds/1.png"
     );
+    setInterval(() => {
+      this.x = this.x - this.v;
+    }, this.dt);
   }
 }
