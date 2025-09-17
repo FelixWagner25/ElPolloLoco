b66b1632e02a277f6925c9c4e21458ea80f9c3ab
Felix Wagner <post@felixwagner.eu>
2025-09-17 17:14:56 +0200

    feat: add keyboard object for arrow and space keys

--- js/game.js
+++ js/game.js
@@ -2,10 +2,50 @@ let canvas;
 let ctx;
 let world;
 let canvasHeightPx = 480;
+let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById("canvas");
   world = new World(canvas);
-
-  console.log("My character is", world.character);
 }
+
+window.addEventListener("keydown", (event) => {
+  //Object.keys(keyboard).forEach((key) => (keyboard[key] = false));
+  switch (event.key) {
+    case "ArrowUp":
+      keyboard.up = true;
+      break;
+    case "ArrowDown":
+      keyboard.down = true;
+      break;
+    case "ArrowRight":
+      keyboard.right = true;
+      break;
+    case "ArrowLeft":
+      keyboard.left = true;
+      break;
+    case " ":
+      keyboard.space = true;
+  }
+  console.log(keyboard);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      keyboard.up = false;
+      break;
+    case "ArrowDown":
+      keyboard.down = false;
+      break;
+    case "ArrowRight":
+      keyboard.right = false;
+      break;
+    case "ArrowLeft":
+      keyboard.left = false;
+      break;
+    case " ":
+      keyboard.space = false;
+  }
+  console.log(keyboard);
+});
