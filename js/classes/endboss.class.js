f0e3bc545b4e255a219f34b7ce9adf5ea9e4017d
Felix Wagner <post@felixwagner.eu>
2025-09-22 15:47:15 +0200

    feat: insert endboss

---
+++ js/classes/endboss.class.js
@@ -0,0 +1,39 @@
+class Endboss extends MovableObject {
+  height = 350;
+  width = 200;
+  y = canvasHeightPx - this.height - 20;
+  x = 2500;
+  v = 0;
+
+  imgsWalking = [
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G5.png",
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G6.png",
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G7.png",
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G8.png",
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G9.png",
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G10.png",
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G11.png",
+    "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G12.png",
+  ];
+  currentImgIndx = 0;
+
+  constructor() {
+    super();
+    this.loadImage(
+      "../../img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G5.png"
+    );
+    this.loadImages(this.imgsWalking);
+    this.walking();
+  }
+
+  walking() {
+    this.moveLeft();
+
+    setInterval(() => {
+      this.currentImgIndx = this.currentImgIndx % this.imgsWalking.length;
+      let path = this.imgsWalking[this.currentImgIndx];
+      this.img = this.imgsCache[path];
+      this.currentImgIndx++;
+    }, 1000);
+  }
+}
--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,5 +1,5 @@
 const level1 = new Level(
-  [new Chicken(), new Chicken(), new Chicken()],
+  [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
   [new Cloud(1), new Cloud(2)],
   5
 );
