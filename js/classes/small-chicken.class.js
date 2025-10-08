959ae5155a70c86a116be715b924ea3814f1e5ae
Felix Wagner <post@felixwagner.eu>
2025-10-08 21:34:19 +0200

    feat: add small-chicken enemies

---
+++ js/classes/small-chicken.class.js
@@ -0,0 +1,32 @@
+class SmallChicken extends MovableObject {
+  height = 50;
+  width = 50;
+  y = canvasHeightPx - this.height - 40;
+  x = 200 + Math.random() * 500;
+  v = 0.15 + Math.random() * 0.25;
+
+  imgsWalking = [
+    "../../img/img_pollo_locco/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "../../img/img_pollo_locco/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "../../img/img_pollo_locco/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+  ];
+
+  constructor() {
+    super();
+    this.loadImage(
+      "../../img/img_pollo_locco/3_enemies_chicken/chicken_small/1_walk/1_w.png"
+    );
+    this.loadImages(this.imgsWalking);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+    }, this.dt);
+
+    setInterval(() => {
+      this.playAnimation(this.imgsWalking);
+    }, 1000);
+  }
+}
--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,5 +1,13 @@
 const level1 = new Level(
-  [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
+  [
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    new SmallChicken(),
+    new SmallChicken(),
+    new SmallChicken(),
+    new Endboss(),
+  ],
   [new Cloud(1), new Cloud(2)],
   [new Bottle(), new Bottle(), new Bottle(), new Bottle()],
   [new Coin(), new Coin(), new Coin(), new Coin()],
