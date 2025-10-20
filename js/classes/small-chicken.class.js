5de93441c4e0642ed596a185ddfca2d2f94c6df0
Felix Wagner <post@felixwagner.eu>
2025-10-20 17:01:23 +0200

    feat: animate death of small chicken

--- js/classes/small-chicken.class.js
+++ js/classes/small-chicken.class.js
@@ -26,7 +26,16 @@ class SmallChicken extends MovableObject {
     }, this.dt);
 
     setInterval(() => {
-      this.playAnimation(this.imgsWalking);
+      if (this.isDead()) {
+        this.loadImage(
+          "../../img/img_pollo_locco/3_enemies_chicken/chicken_small/2_dead/dead.png"
+        );
+        setTimeout(() => {
+          this.loadImage("");
+        }, 2000);
+      } else {
+        this.playAnimation(this.imgsWalking);
+      }
     }, 1000);
   }
 }
