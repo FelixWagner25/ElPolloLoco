1ee4ec0fb97ea518ebc20d4554bf0d65d71be872
Felix Wagner <post@felixwagner.eu>
2025-10-28 11:50:19 +0100

    feat: add offsets to chicken enemies

--- js/classes/small-chicken.class.js
+++ js/classes/small-chicken.class.js
@@ -4,6 +4,12 @@ class SmallChicken extends MovableObject {
   y = canvasHeightPx - this.height - 40;
   x = 200 + Math.random() * 500;
   v = 0.15 + Math.random() * 0.25;
+  offset = {
+    top: 5,
+    bottom: 5,
+    left: 5,
+    right: 10,
+  };
 
   imgsWalking = [
     "../../img/img_pollo_locco/3_enemies_chicken/chicken_small/1_walk/1_w.png",
