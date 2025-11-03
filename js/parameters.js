a14aa242ad0bda2ee3025b67dc2927703bd656e5
Felix Wagner <post@felixwagner.eu>
2025-11-03 11:26:25 +0100

    feat: extend level objects on whole map size

--- js/parameters.js
+++ js/parameters.js
@@ -3,3 +3,4 @@ let dtGameDynamic = 100;
 let dtUserAction = 250;
 let groundLevel = 440; //character.y + character.height - character.offset.bottom
 let sectionLengthPx = 360; // 0.5 * canvasWidth = 0.5 * 720px = 360px
+let numberBackgroundLayers = 5;
