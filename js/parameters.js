fc7119c70643714eee7c28c390bf498450195425
Felix Wagner <post@felixwagner.eu>
2025-11-25 14:05:08 +0100

    fix: do not play bottle break audio when game muted and only throw 2 bottles per second

--- js/parameters.js
+++ js/parameters.js
@@ -1,6 +1,6 @@
 let endgameTrigger = 2000;
 let dtGameDynamic = 100;
-let dtUserAction = 250;
+let dtUserAction = 500;
 let groundLevel = 440; //character.y + character.height - character.offset.bottom
 let sectionLengthPx = 360; // 0.5 * canvasWidth = 0.5 * 720px = 360px
 let numberBackgroundLayers = 5;
