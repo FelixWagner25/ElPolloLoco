5753ef477918906baa1532bd25d3b4e1fe385957
Felix Wagner <post@felixwagner.eu>
2025-12-07 16:57:31 +0100

    refactor: isColliding method

--- js/classes/movable-object.class.js
+++ js/classes/movable-object.class.js
@@ -86,10 +86,10 @@ class MovableObject extends DrawableObject {
     const objTop = obj.y + obj.offset.top;
     const objBottom = obj.y + obj.height - obj.offset.bottom;
     return (
-      this.x + this.width - this.offset.right > obj.x + obj.offset.left &&
-      this.x + this.offset.left < obj.x + obj.width - obj.offset.right &&
-      this.y + this.height - this.offset.bottom > obj.y + obj.offset.top &&
-      this.y + this.offset.top < obj.y + obj.height - obj.offset.bottom
+      thisRight > objLeft &&
+      thisLeft < objRight &&
+      thisBottom > objTop &&
+      thisTop < objBottom
     );
   }
 
